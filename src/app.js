38965b6dfa87b6700f35824a83db5f3ab95a0365
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-12-10 18:55:28 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,5 +2,27 @@ const Blink1 = require('node-blink1');
 
 // array of device serial numbers
 const devices = Blink1.devices();
-
 console.log(devices);
+
+// create blink 1 object
+const blink1 = new Blink1();
+
+const fadeIngreen = () => {
+  blink1.setRGB(50, 200, 100);
+};
+
+var isOn = false;
+
+// blink green every second
+setInterval(() => {
+  if (isOn) {
+    fadeIngreen();
+  }
+  // turn off
+  else {
+    blink1.off();
+  }
+
+  // toggle switch
+  isOn = !isOn;
+}, 1000);
